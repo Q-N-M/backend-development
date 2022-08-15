294f2794b913a80665e7f00661ef088a3348b63a
HaroldHS <haroldhs.hh@gmail.com>
2022-08-15 14:24:29 +1000

    Create first Django REST Framework endpoint

--- Wellderly/main/views.py
+++ Wellderly/main/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+#from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import UserSerializer
+from .models import User
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all().order_by('name')
+    serializer_class = UserSerializer
