fed701593bfcf3fbd6668d414f2f33508e4569d4
HaroldHS <user@HaHS.localdomain>
2022-08-15 13:33:28 +1000

    Setting Django REST Framework

--- Wellderly/Wellderly/settings.py
+++ Wellderly/Wellderly/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -98,6 +100,14 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+# Settings Rest API framework
+# https://www.django-rest-framework.org/
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
 
 # Internationalization
