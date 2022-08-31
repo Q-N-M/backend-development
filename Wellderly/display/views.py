fe5459c99c59323aae14f4a17243808d3917c560
HaroldHS <haroldhs.hh@gmail.com>
2022-08-31 12:51:51 +1000

    Creating dynamic display page

--- Wellderly/display/views.py
+++ Wellderly/display/views.py
@@ -1,4 +1,51 @@
 from django.shortcuts import render
+from main.models import UserEmoji
+
+from datetime import date
 
 def index(request):
-    return render(request, 'desktop2.html')
+    res = {
+        "Angry" : 0,
+        "Sad" : 0,
+        "Neutral" : 0,
+        "Happy" : 0,
+        "Ecstatic" : 0,
+        "TotalUser" : 0,
+        "Percentage" : "",
+        "Time" : "",
+        "Location" : ""
+    }
+
+    # Get time
+    today = date.today().strftime("Today, %B %d %Y")
+    today_split = today.split(" ")
+    if today_split[2][-1] == "1":
+        today_split[2] = today_split[2] + "st"
+    elif today_split[2][-1] == "2":
+        today_split[2] = today_split[2] + "nd"
+    elif today_split[2][-1] == "3":
+        today_split[2] = today_split[2] + "rd"
+    else:
+        today_split[2] = today_split[2] + "th"
+    res["Time"] = " ".join(today_split)
+
+    res["Location"] = "Blue Care Tangara Retirement Village"
+
+    user_emoji = UserEmoji.objects.all()
+    for user in user_emoji:
+        res["TotalUser"] += 1
+        user_emoji_key = user.emoji
+        if user_emoji_key.name == "Angry":
+            res["Angry"] += 1
+        if user_emoji_key.name == "Sad":
+            res["Sad"] += 1
+        if user_emoji_key.name == "Neutral":
+            res["Neutral"] += 1
+        if user_emoji_key.name == "Happy":
+            res["Happy"] += 1
+        if user_emoji_key.name == "Ecstatic":
+            res["Ecstatic"] += 1
+
+    res["Percentage"] = str(int(res["Happy"] / res["TotalUser"] * 100))
+
+    return render(request, 'desktop2.html', res)
--- Wellderly/main/models.py
+++ Wellderly/main/models.py
@@ -15,13 +15,22 @@ class User(models.Model):
     emergency_contact = models.CharField(max_length=256, null=True)
     birth_date = models.DateField(null=True)
 
+    def __str__(self):
+        return self.name
+
 class Community(models.Model):
     id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.name
+
 class Transport(models.Model):
     id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
 
 class Activity(models.Model):
     id = models.BigAutoField(primary_key=True)
@@ -36,9 +45,14 @@ class Emoji(models.Model):
     id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.name
+
 class UserEmoji(models.Model):
     id = models.BigAutoField(primary_key=True)
     emoji = models.ForeignKey(Emoji, on_delete=models.CASCADE)
     date = models.DateField(auto_now_add=False, null = True)
     name = models.CharField(max_length=200,null = True)
 
+    def __str__(self):
+        return self.name
