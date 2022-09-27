a38e01f3e059ca694b61567e9822f46758345a4c
rayhan-muzakki <rayhanmuzakki1@gmail.com>
2022-09-27 18:38:35 +1000

    update settings.py

--- Wellderly/Wellderly/settings.py
+++ Wellderly/Wellderly/settings.py
@@ -152,7 +152,11 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_TMP = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
+os.makedirs(STATIC_TMP, exist_ok=True)
+os.makedirs(STATIC_ROOT, exist_ok=True)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
