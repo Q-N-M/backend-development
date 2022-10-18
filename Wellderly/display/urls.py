2785e950fbee91b4387aa23bf9535de6c7a80806
rayhan-muzakki <rayhanmuzakki1@gmail.com>
2022-10-19 03:16:28 +1000

    push working project

--- Wellderly/display/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import include, path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name="index"),
-    path('home', views.emoji, name="home"),
-    path('display', views.index, name="home"),
-]
